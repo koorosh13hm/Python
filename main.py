6128de887fd11212684c4a9329afa1df38fc3354
Koorosh Sadeghi Tehran <koorosh13hm@gmail.com>
2023-03-19 14:23:18 +0330

    Experiencing with match case.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import json
 
 database_file = {}
 
+
 def add():
   word = input("Enter the word:\n\t--> ")
   description = input("Enter the description2:\n\t--> ")
@@ -14,16 +15,25 @@ def practice():
 
 
 def ender():
+  flag = False
   print("Bye.")
 
+
+available_choices = ["1", "2", "3"]
 flag = True
 while flag:
   choice = input("Choose your option:\n\t1. Add\n\t2. Read\n\t3. Quit\n>>> ")
-  if choice == str(3):
-    flag = False
-    ender()
-    continue
-  elif choice == str(1):
-    add()
+
+  if choice in available_choices:
+    match choice:
+      case "3":
+        ender()
+        break
+      case "1":
+        add()
+        continue
+      case "2":
+        practice()
+        continue
   else:
-    practice()
+    print("No valid choice entered. Try again...")
