0e0cfe98f037d8c4ac5fcf8f551d5c6059e3b4aa
Koorosh Sadeghi Tehran <koorosh13hm@gmail.com>
2023-06-23 16:05:10 +0330

    some practice on learning the differece between python 2 and 3 for calculating on numbers

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-print("Hello, Python...")
+# print("Hello, Python...")
 # -----------------------------------------------
 
-name = input("Enter Name: ")
-print(f"Welcome, {name}.")
+# In python 3.10.5 (On Windows)
+print(1 / 2)
+print(int(1 / 2))
+print(10 / 2)
+print(int(10 / 2))
+print(1.0 / 2)
+print(10 / 2.0)
 
-age = input("Enter Age: ")
-print(f"{name}'s age number type:", type(age))
-print(':', age)
-age_float = float(age)
-print(f"{name}'s age number type after conversion:", type(age_float))
-print(':', age_float)
 
-age_int = int(age_float)
-print(f"{name}'s age number type after conversion:", type(age_int))
-print(':', age_int)
+# The first command in Python 2 yields 0
+print(int(99 / 100))
+print(99 / 100)
+
