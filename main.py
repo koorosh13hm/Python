7b6fe93203e42a1c7e87f04f613d1bc0fb50bcc2
Koorosh Sadeghi Tehran <koorosh13hm@gmail.com>
2023-06-23 15:10:54 +0330

    Type conversion

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 print("Hello, Python...")
+# -----------------------------------------------
+
+name = input("Enter Name: ")
+print(f"Welcome, {name}.")
+
+age = input("Enter Age: ")
+print(f"{name}'s age number type:", type(age))
+print(':', age)
+age_float = float(age)
+print(f"{name}'s age number type after conversion:", type(age_float))
+print(':', age_float)
+
+age_int = int(age_float)
+print(f"{name}'s age number type after conversion:", type(age_int))
+print(':', age_int)
