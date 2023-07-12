56b86c8798aafac39d104eb2b34ca546c5a0ed01
Koorosh Sadeghi Tehran <koorosh13hm@gmail.com>
2023-07-12 21:14:11 +0330

    Trying to resolve conflict

--- Learning_&_Practices/test_module.py
+++ Learning_&_Practices/test_module.py
@@ -49,11 +49,97 @@ print("\n")
 
 print("-----------------------------------------------------------------")
 """ Builtin String Functions: variable.title() """
-print("title() function for both first name and last_name "
-      "variables:\n\t" + first_name.title() +" "+
+print("title() function for both first name and last_name ",
+      "variables:\n\t" + first_name.title() + " " +
       last_name.title())
 print("\n")
 
 
 print("-----------------------------------------------------------------")
-""" Builtin String Functions: variable.title() """
+""" Builtin String Functions: variable.upper() """
+print("upper() function for both first name and last_name ",
+      "variables:\n\t" + first_name.upper() + " " +
+      last_name.upper())
+print("\n")
+
+
+print("-----------------------------------------------------------------")
+""" Builtin String Functions: variable.lower() """
+print("lower() function for both first name and last_name ",
+      "variables:\n\t" + first_name.lower() + " " +
+      last_name.lower())
+print("\n")
+
+
+print("-----------------------------------------------------------------")
+""" Builtin String Functions: variable.find() """
+print("find() function for both first name and last_name ",
+      "variables:\n\t" + str(first_name.find("k")) + " " +
+      str(last_name.find("t")))
+print("\n")
+
+
+print("-----------------------------------------------------------------")
+""" Builtin String Functions: variable.isdigit() """
+name_1 = "Barcelona"
+name_2 = "2675874"
+name_3 = "K1O2O3R4O5S6H7"
+print("name_1.isdigit():", name_1.isdigit())
+print("name_2.isdigit():", name_2.isdigit())
+print("name_2.isdigit():", name_2.isdigit())
+print("\n")
+
+
+print("-----------------------------------------------------------------")
+""" Builtin String Functions: variable.isalpha() """
+# To check if your string contains alphabetical letters
+name_1 = "Barcelona"
+name_2 = "2675874"
+name_3 = "K1O2O3R4O5S6H7"
+print("name_1.isdigit():", name_1.isdigit())
+print("name_2.isdigit():", name_2.isdigit())
+print("name_2.isdigit():", name_2.isdigit())
+print("\n")
+
+
+print("-----------------------------------------------------------------")
+""" Builtin String Functions: variable.capitalize() """
+name = "KOOROSH SADEGHI TEHRAN"
+print(name.capitalize())
+
+
+print("-----------------------------------------------------------------")
+""" Builtin String Functions: variable.count() """
+name = "KOOROSH SADEGHI TEHRAN"
+print("Count of `O`:", name.count("o"))
+print("Count of `O`:", name.count("O"))
+
+
+print("-----------------------------------------------------------------")
+""" Builtin String Functions: variable.replace("which", "with what") """
+""" Builtin String Functions: variable.count() """
+name = "KOOROSH SADEGHI TEHRAN"
+print("After replacing `o` with `a`:", name.replace("o", "a"))
+print("After replacing `O` with `X`:", name.replace("O", "X"))
+
+
+print("-----------------------------------------------------------------")
+""" A very cool trick to print many of a sequence """
+print(8 * "%")
+print(8 * "@@@@@@@@")
+print(name * 5)
+
+
+print("-----------------------------------------------------------------")
+"""  """
+
+
+
+
+
+
+
+
+"""
+print("-----------------------------------------------------------------")
+"""
